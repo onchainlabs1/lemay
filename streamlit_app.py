b57048f760ad7e97bce289d5d99c1d20374d457d
onchainlabs1 <146457568+onchainlabs1@users.noreply.github.com>
2024-05-14 10:53:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,17 @@ import streamlit as st
 import requests
 
 def main():
-    st.title('Interface de Análise de Sentimento')
-    user_input = st.text_area('Digite um texto para analisar o sentimento:', height=150)
-    if st.button('Analisar Sentimento'):
+    st.title('Sentiment Analysis Interface')
+    user_input = st.text_area('Type a text to analyze sentiment:', height=150)
+    if st.button('Analyze Sentiment'):
         response = requests.post('http://localhost:8000/predict', json={'text': user_input})
         if response.status_code == 200:
-            st.write('Resultado da análise:')
+            st.write('Analysis Result:')
             st.write(response.json())
         else:
-            st.error('Falha ao processar a solicitação.')
+            st.error('Failed to process the request.')
 
 if __name__ == '__main__':
     main()
+
 
